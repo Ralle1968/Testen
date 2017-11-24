850627c66c266bea2778b85e6afbd005403a1cf7
Ralle1968 <graef.ralf@gmail.com>
2017-11-24 22:56:18 +0100

    first silly change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in, asshole!
+                    You are logged in, super asshole!
                 </div>
             </div>
         </div>
